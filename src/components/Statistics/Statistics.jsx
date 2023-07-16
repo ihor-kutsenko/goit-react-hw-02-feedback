1565f56becb3a45e459b0d9637fa0407179674ab
Rammbest <igor-kutsenko@ukr.net>
2023-07-16 16:13:01 +0300

    add render components on page

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -3,12 +3,12 @@ import css from './Statistics.module.css';
 
 const Statistics = ({ good, neutral, bad, total = 0, positivePercentage = 0 }) => {
   return (
-    <div className={css.statistics}>
-      <p className={css.item}>Good: {good}</p>
-      <p className={css.item}>Neutral: {neutral}</p>
-      <p className={css.item}>Bad: {bad}</p>
-      <p className={css.item}>Total: {total}</p>
-      <p className={css.item}>Positive feedback: {positivePercentage}%</p>
+    <div>
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
+      <p>Total: {total}</p>
+      <p>Positive feedback: {positivePercentage}%</p>
     </div>
   );
 };
